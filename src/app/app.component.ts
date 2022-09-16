e9c4641a6a7c3458bf1f99028436ecbee7f790c9
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-09-17 00:30:09 +0200

    Fixed theme bug in Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OverlayContainer } from "@angular/cdk/overlay";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Johannes Hölllwerth';
+
+  constructor(private overlay: OverlayContainer) { }
 
   getDarkmode(): boolean {
     return localStorage.getItem('theme') === 'darkmode';
+  }
+
+  ngOnInit(): void {
+    const darkClassName = 'darkMode';
+
+    if (localStorage.getItem('theme') === 'darkmode') {
+      this.overlay.getContainerElement().classList.add(darkClassName);
+      return;
+    }
+    this.overlay.getContainerElement().classList.remove(darkClassName);
   }
 }
