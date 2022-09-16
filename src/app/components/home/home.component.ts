c62426bc5924744fee8b5e19ca07e63836b807ea
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-09-17 00:24:05 +0200

    Added HomeComponent

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.animateContent();
+  }
+
+  animateContent(): void {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+          return;
+        }
+        entry.target.classList.remove('show')
+      });
+    });
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+  }
+}
