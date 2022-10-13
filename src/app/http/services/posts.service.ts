dbd003fcbd22ac473eed2827bb24b95efba6b215
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-10-13 22:47:42 +0200

    Added PostService

---
+++ src/app/http/services/posts.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../../environments/environment";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor(private readonly http: HttpClient) { }
+
+  getAllPosts(): Observable<any> {
+    return this.http.get(`${environment.domain}post/`);
+  }
+
+  createPost(post: object) {
+    return this.http.post(`${environment.domain}post/`, post);
+  }
+}
