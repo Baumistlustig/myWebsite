33558405648f07c39fefaf0fd6dbe8acbb4ba5e6
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-10-14 23:11:28 +0200

    Added UserService

---
+++ src/app/http/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private readonly http: HttpClient) { }
+
+  // EditUser
+  editUser(username: string, email: string, password: string): Observable<object> {
+    return this.http.patch(`${environment.domain}user/edit`,
+      { new_username: username, new_password: password });
+  }
+
+  // GetUser
+  getUser(id: string | null): Observable<any> {
+    return this.http.get(`${environment.domain}user/getuser/${id}`);
+  }
+}
