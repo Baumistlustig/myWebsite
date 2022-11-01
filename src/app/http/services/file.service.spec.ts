919e44594344e6b36d73013968f34944a7afb04e
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-11-01 23:43:23 +0100

    Added FileServiceTest

---
+++ src/app/http/services/file.service.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed } from '@angular/core/testing';
+
+import { FileService } from './file.service';
+import { HttpClientModule } from '@angular/common/http';
+
+describe('FileService', () => {
+  let service: FileService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [FileService]
+    });
+    service = TestBed.inject(FileService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return an array of all fileIds', () => {
+    expect(service.getFiles()).toBeTruthy();
+  });
+});
